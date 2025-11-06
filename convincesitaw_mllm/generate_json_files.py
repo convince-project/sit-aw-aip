4e6db4943c53d5bb93a9eaea52bec9ff54879601
AZROU Razane 274338 <ra274338@is247452.intra.cea.fr>
2025-11-06 18:15:37 +0100

    code changes

--- convincesitaw_mllm/generate_json_files.py
+++ convincesitaw_mllm/generate_json_files.py
@@ -1,11 +1,11 @@
 #SIT-AW  Copyright (C) CEA 2025  Razane Azrou
-from convincesitaw_mllm.inference.Ucs_mapping import use_case
+from convincesitaw_mllm.inference.Ucs_mapping import use_case_map
 import tyro
 
 
 def main(use_case_id:int,root_path:str):
 
-   use_case[use_case_id].main(root_path)
+   use_case_map[use_case_id].main(root_path)
 
 def cli():
     tyro.cli(main)
